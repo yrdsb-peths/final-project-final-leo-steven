477194dcf39c40cf3c6837b07ee842d6cf0c0198
Steven542430 <440037784@gapps.yrdsb.ca>
2025-05-20 14:48:16 -0400

    Added metor class

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,9 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
+public class MyWorld extends World 
+{
+    public MyWorld() 
+    {
         super(525,750, 1);
     }
 }
