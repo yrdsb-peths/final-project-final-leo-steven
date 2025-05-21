314e5a9b02511d902e390bdce22351d74135435f
Steven542430 <440037784@gapps.yrdsb.ca>
2025-05-21 14:39:44 -0400

    Added metro moving

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,18 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
+public class MyWorld extends World 
+{
+    public MyWorld() 
+    {
         super(525,750, 1);
+        createm();
+    }
+    
+    public void createm()
+    {
+        Metro m = new Metro();
+        int x = Greenfoot.getRandomNumber(525);
+        int y = 0;
+        addObject(m, x, y);        
     }
 }
