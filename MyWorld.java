6a93d7b73c58ba08e63534c6740e55a56925659e
LeooSo <sol542939@gmail.com>
2025-05-22 13:50:33 -0400

    Removed jet

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,6 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
-        Jet jet = new Jet();
-        addObject(jet, 300, 300);
+
     }
 }
