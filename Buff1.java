af948ce022b9a0d423379662ee08506e86afabda
Steven542430 <440037784@gapps.yrdsb.ca>
2025-05-30 14:42:38 -0400

    Added buff

---
+++ Buff1.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Buff1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Buff1 extends Actor
+{
+    /**
+     * Act - do whatever the Buff1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (isTouching(Ship.class))
+        {
+            r
+        }
+    }
+}
